bb90d9421fbe240240d83ae33ab644f24a438aa4
Andrzej Mróz <andrzejmroz1999@gmail.com>
2022-08-15 18:00:36 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new DataReader();
+            var path = System.IO.Path.GetDirectoryName(
+      System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6);
+            string fileName = "\\data.csv";
+            reader.ImportAndPrintData(string.Format("{0}{1}", path, fileName));
+        }
+    }
+}
